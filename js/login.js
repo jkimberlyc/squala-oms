60fa7813ee63c1ed38ee2fa20478ae6cc27ed6e8
Kimberly Carriedo <91200385+jkimberlyc@users.noreply.github.com>
2022-07-23 03:58:19 +0800

    login page update

--- js/login.js
+++ js/login.js
@@ -1,3 +1,8 @@
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 var name = document.getElementById('UserName')
 var password = document.getElementById('Password')
 var loginPeople =[{
@@ -31,7 +36,6 @@ for(i=0;i<loginPeople.length;i++){
 }
 alert("login failed");
 }
-
 
     // if(username=="admin" && password=="user"){
     //     alert("login successfully");
